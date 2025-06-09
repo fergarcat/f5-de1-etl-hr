ef4e07a40dff5a548b249d383c06f1226b60c6f7
Fernando García Catalán <fernandogcatalan@gmail.com>
2025-06-09 12:44:30 +0200

    feat(mongo): add mongodb_config.py class MongoDbConnection __new__, connect(), close()

---
+++ config/mongodb_config.py
@@ -0,0 +1,51 @@
+import os
+from pymongo import MongoClient
+from pymongo.database import Database
+from dotenv import load_dotenv
+from logger_config import logger
+import os
+
+# If .env is in the parent directory
+# dotenv_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '.env')
+# print(dotenv_path)
+# load_dotenv(dotenv_path)
+load_dotenv()
+
+
+class MongoDbConnection:
+    _instance = None
+    _client = None
+    _database = None
+    
+    def __new__(cls):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls)
+        return cls._instance
+    
+    def connect(self) -> Database:
+        if self._client is None:
+            try:
+                self._client = MongoClient(
+                    os.getenv('MONGODB_URL'),
+                    username=os.getenv('MONGO_INITDB_ROOT_USERNAME'),
+                    password=os.getenv('MONGO_INITDB_ROOT_PASSWORD'),
+                    serverSelectionTimeoutMS=5000
+                )
+                # Check if the connection is successful
+                self._client.admin.command('ismaster')
+                self._database = self._client[os.getenv('MONGODB_DB_NAME')]
+                logger.info("✅ MongoDB connection established")
+            except Exception as e:
+                logger.error(f"❌ Error connectinig MongoDB: {e}")
+                raise
+        return self._database
+    
+    def close(self):
+        if self._client:
+            self._client.close()
+            self._client = None
+            self._database = None
+
+
+# Global Instance
+db_connection = MongoDbConnection()
